cbcdd044fc69118a19daca47d0cdef3958c1963f
dmod129 <derek.modugno@gmail.com>
2023-10-12 18:09:59 -0500

    created init function

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ const questions = [
         name: 'characters',
         message: 'Please choose 3 characters for your logo.'
     }
-]
+];
+
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        console.log("creating SVG file");
+       createLogo(response);
+    });
+};
+
+init();
