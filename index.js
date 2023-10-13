9dd7c8510bdef63811a3e2bca03b8f8a055e14ff
dmod129 <derek.modugno@gmail.com>
2023-10-13 17:51:05 -0500

    added function createSVG(response)

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const {Triangle, Square, Circle} = require("./lib/shapes");
 
 const questions = [
     {
         type: 'input',
         name: 'characters',
-        message: 'Please choose 3 characters for your logo.'
-    }
+        message: 'Please choose up to 3 characters for your logo.',
+        validate: (input) => input.length <= 3,
+    },
+
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color.',
+    },
 ];
+
+// Create logo
+function createSVG(response) {
+    const {characters, textColor} = response;
+    return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <text x="20" y="50" fill="${textColor}" font-size="24">${characters}</text>
+    </svg>`;
+    };
+    
+
+
+function writeToFile(logo, outputPath) {
+    fs.writeFileSync(outputPath, logo);
+    console.log('SVG file saved');
+};
 
 function init() {
     inquirer.prompt(questions)
     .then((response) => {
         console.log("creating SVG file");
-       createLogo(response);
+        const logo = createSVG(response);
+        writeToFile(logo, "./dist/logo.svg");
     });
 };
 
