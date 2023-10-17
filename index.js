b6adea9aa9290b9e2fd76ccda568fff47b6a8923
dmod129 <derek.modugno@gmail.com>
2023-10-17 15:28:25 -0500

    updated README

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function createSVG(response) {
 
 function writeToFile(logo, outputPath) {
     fs.writeFileSync(outputPath, logo);
-    console.log('SVG file saved');
+    console.log('Generated logo.svg');
 };
 
 function init() {
